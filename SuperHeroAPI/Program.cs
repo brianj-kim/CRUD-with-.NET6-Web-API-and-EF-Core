ffea16c344b57996d001a56072676a93fbeca6a3
BrianJ Kim <brianjkim.dev@google.com>
2023-02-03 11:30:20 -0600

    Add Database Context, Entity Frameworks, and Migration functionality

--- SuperHeroAPI/Program.cs
+++ SuperHeroAPI/Program.cs
@@ -1,20 +1,38 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Migrate latest database changes during startup
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var dbContext = scope.ServiceProvider
+        .GetRequiredService<DbContext>();
+
+    // Here is the migration executed
+    dbContext.Database.Migrate();
 }
+
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -23,4 +41,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
